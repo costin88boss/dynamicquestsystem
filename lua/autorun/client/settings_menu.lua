261032f58b22b91fff49a8b53d233ad5750b1e04
costin88boss <costin211.boss@gmail.com>
2024-10-29 19:57:06 +0200

    add min/max enemies spawn in settings

--- lua/autorun/client/settings_menu.lua
+++ lua/autorun/client/settings_menu.lua
@@ -12,5 +12,8 @@ hook.Add("PopulateToolMenu", "costin_dqs_createtoolmenu", function()
 		panel:NumSlider("Max Spawn Retry Attempts", "costin_dqs_spawnretryattempts", 1, 20, 0)
 		panel:NumSlider("Min Distance from Players", "costin_dqs_mindistancefromplayers", 200, 20000, 0)
 		panel:NumSlider("Max NPC Amount", "costin_dqs_maxnpcamount", 1, 30, 0)
+
+		panel:NumSlider("Min Distance for Quest Enemy spawn", "costin_dqs_enemyspawn_distmax", 500, 20000, 0)
+		panel:NumSlider("Max Distance for Quest Enemy spawn", "costin_dqs_enemyspawn_distmin", 500, 20000, 0)
 	end )
 end )
