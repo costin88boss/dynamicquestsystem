09f9148c23cfad625df909e481e5ef828e2ac712
costin88boss <costin211.boss@gmail.com>
2024-10-29 22:49:57 +0200

    comment fixes

--- lua/entities/npc_dqs_questgiver/cl_init.lua
+++ lua/entities/npc_dqs_questgiver/cl_init.lua
@@ -1,2 +1,2 @@
 // For some odd reason this file is not being run on multiplayer.
-// Everything clientside is in shared.lua because fuck gmod
+// Everything clientside is in shared.lua instead.
--- lua/entities/sent_dqs_triggerbubble/shared.lua
+++ lua/entities/sent_dqs_triggerbubble/shared.lua
@@ -8,8 +8,8 @@ ENT.Category = "Dynamic Quest System"
 
 ENT.Spawnable = false // intentional
 
+// Also intentional as cl_init.lua doesn't get run in multiplayer.
 function ENT:Draw()
-
     if(LocalPlayer() == self:GetNW2Entity("quest_player")) then
         self:DrawModel()
     end
